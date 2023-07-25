5b29b20a42141270eaa18b7ed9e3faeac4fbc113
JadeKim042386 <jk042386@gmail.com>
2023-07-25 21:33:16 +0900

    feat: application rename 및 spring batch 실행 코드 삭제 - project rename에 따라 application도 맞게 rename - application에 작성된 spring batch는 학습 용도이므로 삭제

--- src/main/java/com/spring/pass/PassBatchApplication.java
+++ src/main/java/com/spring/pass/PassBatchApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PassApplication {
-
+public class PassBatchApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PassApplication.class, args);
+        SpringApplication.run(PassBatchApplication.class, args);
     }
-
 }
