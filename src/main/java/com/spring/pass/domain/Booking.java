8d9adb69a9ef77468f14966eb51b966206541021
JadeKim042386 <jk042386@gmail.com>
2023-07-27 04:46:59 +0900

    feat: Booking 도메인 수정 - `UserAccount` 에대한 `@ManytoOne` 연관성 추가 - startedAt, endedAt, canceledAt의 `DateTimeFormat` 지정 - 생성자 메소드 추가

--- src/main/java/com/spring/pass/domain/Booking.java
+++ src/main/java/com/spring/pass/domain/Booking.java
@@ -3,6 +3,7 @@ package com.spring.pass.domain;
 import com.spring.pass.domain.constant.BookingStatus;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -17,23 +18,29 @@ public class Booking extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long passTicketId; //이용권 ID
-    private String userId; //사용자 ID
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private UserAccount userAccount; //사용자
 
     @Enumerated(EnumType.STRING)
     private BookingStatus status; //예약 상태
     private boolean usedPass; //사용 여부
     private boolean attended; //출석 여부
 
+    @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm")
     private LocalDateTime startedAt; //시작 일시
+    @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm")
     private LocalDateTime endedAt; //종료 일시
+    @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm")
     private LocalDateTime cancelledAt; //취소 일시
 
     protected Booking() {
     }
 
-    private Booking(Long passTicketId, String userId, BookingStatus status, boolean usedPass, boolean attended, LocalDateTime startedAt, LocalDateTime endedAt, LocalDateTime cancelledAt) {
+    private Booking(Long passTicketId, UserAccount userAccount, BookingStatus status, boolean usedPass, boolean attended, LocalDateTime startedAt, LocalDateTime endedAt, LocalDateTime cancelledAt) {
         this.passTicketId = passTicketId;
-        this.userId = userId;
+        this.userAccount = userAccount;
         this.status = status;
         this.usedPass = usedPass;
         this.attended = attended;
@@ -42,8 +49,12 @@ public class Booking extends AuditingFields {
         this.cancelledAt = cancelledAt;
     }
 
-    public static Booking of(Long passTicketId, String userId, BookingStatus status, boolean usedPass, boolean attended, LocalDateTime startedAt, LocalDateTime endedAt, LocalDateTime cancelledAt) {
-        return new Booking(passTicketId, userId, status, usedPass, attended, startedAt, endedAt, cancelledAt);
+    public static Booking of(Long passTicketId, UserAccount userAccount, BookingStatus status, boolean usedPass, boolean attended, LocalDateTime startedAt, LocalDateTime endedAt, LocalDateTime cancelledAt) {
+        return new Booking(passTicketId, userAccount, status, usedPass, attended, startedAt, endedAt, cancelledAt);
+    }
+
+    public static Booking of(Long passTicketId, UserAccount userAccount, BookingStatus status, LocalDateTime startedAt, LocalDateTime endedAt) {
+        return Booking.of(passTicketId, userAccount, status, false, false, startedAt, endedAt, null);
     }
 
     @Override
