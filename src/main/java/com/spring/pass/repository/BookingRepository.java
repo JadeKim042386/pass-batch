59d92ea512b8df0c0c5c3994a510ae7bd18ce6a2
JadeKim042386 <jk042386@gmail.com>
2023-07-27 16:29:58 +0900

    feat: 이용권 횟수 차감 기능 구현을 위한 쿼리 메소드 추가 - `BookingRepository`: passTicketId로 usesPass 수정 - `PassTicketRepository`: passTicketId로 remainingCount 수정  #10

--- src/main/java/com/spring/pass/repository/BookingRepository.java
+++ src/main/java/com/spring/pass/repository/BookingRepository.java
@@ -2,6 +2,18 @@ package com.spring.pass.repository;
 
 import com.spring.pass.domain.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE Booking b" +
+            "          SET b.usedPass = :usedPass," +
+            "              b.modifiedAt = CURRENT_TIMESTAMP" +
+            "        WHERE b.passTicket.id = :passTicketId")
+    int updateUsedPass(@Param("passTicketId") Long passTicketId, @Param("usedPass") boolean usedPass);
 }
