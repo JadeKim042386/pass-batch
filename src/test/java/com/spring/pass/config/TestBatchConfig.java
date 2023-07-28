7e8b108fab3a69ba4a624bf53103331cbf84b298
JadeKim042386 <jk042386@gmail.com>
2023-07-28 16:16:23 +0900

    test: TestBatchConfig에서 BatchConfig를 Import하도록 수정

--- src/test/java/com/spring/pass/config/TestBatchConfig.java
+++ src/test/java/com/spring/pass/config/TestBatchConfig.java
@@ -1,15 +1,13 @@
 package com.spring.pass.config;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@TestConfiguration
+@Import(BatchConfig.class)
 @EnableAutoConfiguration
-@EnableBatchProcessing
 @EntityScan("com.spring.pass.domain")
 @EnableJpaRepositories("com.spring.pass.repository")
 @EnableTransactionManagement
